9bf8c70c57c96f19178541e3a7337c5563cf6a91
mwan-629 <mwan12@my.bcit.ca>
2018-11-12 00:57:51 -0800

    Update startcontainer.sh

--- appspec/startcontainer.sh
+++ appspec/startcontainer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 $(aws ecr get-login --no-include-email --region us-west-2)
-sudo docker pull 203417976784.dkr.ecr.us-west-2.amazonaws.com/mwan-629/pywebapp:latest || {
+sudo docker pull 924781870100.dkr.ecr.us-west-2.amazonaws.com/mwan-629/pywebapp:latest || {
     echo "ERROR: docker pull failed. Sleeping for 10 minutes to allow investigation..."
     sleep 600
     exit 1
 }
-sudo docker run --name pywebapp -p 80:8080 --detach 203417976784.dkr.ecr.us-west-2.amazonaws.com/comp4913/pywebapp:latest
+sudo docker run --name pywebapp -p 80:8080 --detach 924781870100.dkr.ecr.us-west-2.amazonaws.com/mwan-629/pywebapp:latest
